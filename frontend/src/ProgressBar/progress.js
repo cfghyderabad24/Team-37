da1544e82a145246678b148d6179a15ef3c70dcc
Ankit Kumar Singh <ankitdiswar10@gmail.com>
2024-06-22 22:11:27 +0530

    added  progressbar

---
+++ frontend/src/ProgressBar/progress.js
@@ -0,0 +1,8 @@
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import 'react-bootstrap/ProgressBar';
+
+function Progress() {
+  return <ProgressBar now={60} />;
+}
+
+export default Progress;
