009397cc90468782df9ec679d0a71b32e0d30030
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2024-06-22 19:10:56 +0530

    login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/Login.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
+import "./App.css";
+
+const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const navigate = useNavigate();
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const onButtonClick = () => {
+    let valid = true;
+
+    if (!validateEmail(email)) {
+      setEmailError("Please enter a valid email.");
+      valid = false;
+    } else {
+      setEmailError("");
+    }
+
+    if (password.length < 6) {
+      setPasswordError("Password must be at least 6 characters.");
+      valid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    if (valid) {
+      // Perform login logic here (e.g., API call)
+      // If login is successful, navigate to the next page
+      navigate("/home");
+    }
+  };
+
+  return (
+    <div className={"mainContainer"}>
+      <div className={"titleContainer"}>
+        <div>Login</div>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          value={email}
+          placeholder="Enter your email here"
+          onChange={(ev) => setEmail(ev.target.value)}
+          className={"inputBox"}
+        />
+        <label className="errorLabel">{emailError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          type="password"
+          value={password}
+          placeholder="Enter your password here"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className={"inputBox"}
+        />
+        <label className="errorLabel">{passwordError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"Log in"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
