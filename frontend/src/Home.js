20037d029fdb5807c4015637dfecb93ae4f62cf0
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2024-06-23 01:45:02 +0530

    .

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import UploadForm from "./UploadForm";
 // index.js or App.js
 import "bootstrap/dist/css/bootstrap.min.css";
 import Progress from "./progress";
@@ -7,10 +7,9 @@ function Home() {
   return (
     <div className="home">
       <Progress />
-      <form action="/upload" method="POST" enctype="multipart/form-data">
-        <input type="file" name="profileImage" />
-        <button type="submit">Upload</button>
-      </form>
+      <div>
+        <UploadForm />
+      </div>
     </div>
   );
 }
